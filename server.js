0b8cc985d131515d17851ec31a9981e4a9ba1fec
youngerheart <435495400@qq.com>
2015-07-01 15:38:00 +0800

    add server

---
+++ server.js
@@ -0,0 +1,10 @@
+
+config = {
+  'localhost': {
+    backend: __dirname + '/api/',
+    frondend: __dirname + '/web/',
+    baseTemp: 'index.html'
+  }
+}
+var server = require('web-node-server');
+server.start(config);
